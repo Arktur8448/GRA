3be58423eecb0459236260a3e68d3e48287ebe4d
Artur <amergalski@zsnr1.com>
2024-01-05 20:45:04 +0100

    Nie pytaj

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pyglet
-print("Witaj Okrótny Świecie")
-for i in range(10):
-    print("*")
-# The missile knows where it is at any given moment
+import  time
+import random
+window = pyglet.window.Window(resizable=True)
+
+@window.event
+def on_resize(width, height):
+    print(f'The window was resized to {width},{height}')
+image = pyglet.resource.image('kitten.png')
+@window.event
+def on_draw():
+    window.clear()
+    image.blit(0, 0)
+@window.event
+def on_key_press(symbol, modifiers):
+    window.set_location(random.randint(0,1900), random.randint(0,1000))
+
+pyglet.app.run()
