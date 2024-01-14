67494c87267db7098fb66ff60d65a1aea523ccf9
Artur <amergalski@zsnr1.com>
2024-01-14 10:41:21 +0100

    Dodanie Sprintu i Przeniesienie Movement do pliku player.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import arcade
 import player as pl
-from pyglet.math import Vec2
-import time
 
 SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 540
 SCREEN_TITLE = "GAME"
-CAMERA_SPEED = 0.05 # szybokość z jaką kamera nadąża za graczem od 0 do 1
+CAMERA_SPEED = 0.05  # szybokość z jaką kamera nadąża za graczem od 0 do 1
 
 
 class Game(arcade.Window):
@@ -14,7 +12,7 @@ class Game(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        arcade.set_background_color((231,255,80))
+        arcade.set_background_color((231, 255, 80))
 
         self.playerObject = None
 
@@ -25,20 +23,17 @@ class Game(arcade.Window):
 
         self.camera = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.dodge_cooldown = 0
-        self.doge_last_time = 0
-
     def setup(self):
-        """ Set up the game variables. Call to re-start the game. """
         self.tile_map = arcade.load_tilemap("Maps/village/village.json", 2)
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
         self.scene.add_sprite_list("Player")
+        if self.tile_map.background_color:
+            arcade.set_background_color(self.tile_map.background_color)
 
-        self.playerObject = pl.Player(735, 865, arcade.Sprite("Player.png",0.75))  # tworzenie obietu gracza
+        self.playerObject = pl.Player(735, 865, arcade.Sprite("Player.png", 0.75))  # tworzenie obietu gracza
 
         self.playerObject.update_pos()
-        self.dodge_cooldown = self.playerObject.dodge_cooldown
-        self.doge_last_time = time.perf_counter() - self.playerObject.dodge_cooldown
+
         self.scene.add_sprite("Player", self.playerObject.playerSprite)
 
         # Utworzenie silnkia fizyki nakładającego kolizje na Walls
@@ -46,15 +41,12 @@ class Game(arcade.Window):
             self.playerObject.playerSprite, self.scene.get_sprite_list("collision")
         )
 
-        # Set up the player
-
     def on_draw(self):
         """
         Render the screen.
         """
         self.clear()
 
-        # Call draw() on all your sprite lists below
         self.scene.draw()
 
         self.camera.use()
@@ -65,94 +57,13 @@ class Game(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        self.movement()
+        self.playerObject.movement(self.camera, CAMERA_SPEED, SCREEN_WIDTH, SCREEN_HEIGHT, self.physics_engine)
 
     def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
-        """
         self.playerObject.keys[key] = True
 
     def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
         del self.playerObject.keys[key]
-
-    # Własne funkcje
-    def movement(self):
-        def check_move_key():  # aktualizacja pozycji w zależności od naciśniętych klawiszy
-            if arcade.key.W in self.playerObject.keys:
-                self.playerObject.playerSprite.change_y = self.playerObject.movement_speed
-                self.playerObject.direction_move = "Up"
-            elif arcade.key.S in self.playerObject.keys:
-                self.playerObject.playerSprite.change_y = -self.playerObject.movement_speed
-                self.playerObject.direction_move = "Down"
-            else:
-                self.playerObject.playerSprite.change_y = 0
-
-            if arcade.key.A in self.playerObject.keys:
-                self.playerObject.playerSprite.change_x = -self.playerObject.movement_speed
-                if arcade.key.W in self.playerObject.keys:
-                    self.playerObject.direction_move = "UpLeft"
-                elif arcade.key.S in self.playerObject.keys:
-                    self.playerObject.direction_move = "DownLeft"
-                else:
-                    self.playerObject.direction_move = "Left"
-            elif arcade.key.D in self.playerObject.keys:
-                self.playerObject.playerSprite.change_x = self.playerObject.movement_speed
-                if arcade.key.W in self.playerObject.keys:
-                    self.playerObject.direction_move = "UpRight"
-                elif arcade.key.S in self.playerObject.keys:
-                    self.playerObject.direction_move = "DownRight"
-                else:
-                    self.playerObject.direction_move = "Right"
-            else:
-                self.playerObject.playerSprite.change_x = 0
-
-            if arcade.key.SPACE in self.playerObject.keys:
-                if time.perf_counter() - self.doge_last_time > self.playerObject.dodge_cooldown:
-                    dodge()
-
-        def move_camera_to_player():
-            position = Vec2(
-                self.playerObject.playerSprite.center_x - self.width / 2,
-                self.playerObject.playerSprite.center_y - self.height / 2
-            )
-            self.camera.move_to(position, CAMERA_SPEED)
-
-        def dodge():
-            self.doge_last_time = time.perf_counter()
-            if self.playerObject.direction_move == "Left":
-                self.playerObject.playerSprite.change_x = -self.playerObject.movement_speed * self.playerObject.dodge_distance
-            elif self.playerObject.direction_move == "UpLeft":
-                self.playerObject.playerSprite.change_y = self.playerObject.movement_speed * self.playerObject.dodge_distance
-                self.playerObject.playerSprite.change_x = -self.playerObject.movement_speed * self.playerObject.dodge_distance
-            elif self.playerObject.direction_move == "Up":
-                self.playerObject.playerSprite.change_y = self.playerObject.movement_speed * self.playerObject.dodge_distance
-
-            elif self.playerObject.direction_move == "UpRight":
-                self.playerObject.playerSprite.change_y = self.playerObject.movement_speed * self.playerObject.dodge_distance
-                self.playerObject.playerSprite.change_x = self.playerObject.movement_speed * self.playerObject.dodge_distance
-
-            elif self.playerObject.direction_move == "Right":
-                self.playerObject.playerSprite.change_x = self.playerObject.movement_speed * self.playerObject.dodge_distance
-
-            elif self.playerObject.direction_move == "DownRight":
-                self.playerObject.playerSprite.change_y = -self.playerObject.movement_speed * self.playerObject.dodge_distance
-                self.playerObject.playerSprite.change_x = self.playerObject.movement_speed * self.playerObject.dodge_distance
-
-            elif self.playerObject.direction_move == "Down":
-                self.playerObject.playerSprite.change_y = -self.playerObject.movement_speed * self.playerObject.dodge_distance
-                
-            elif self.playerObject.direction_move == "DownLeft":
-                self.playerObject.playerSprite.change_y = -self.playerObject.movement_speed * self.playerObject.dodge_distance
-                self.playerObject.playerSprite.change_x = -self.playerObject.movement_speed * self.playerObject.dodge_distance
-
-        check_move_key()
-        self.physics_engine.update()
-        self.playerObject.update_pos()
-        move_camera_to_player()
 
 
 def main():
--- player.py
+++ player.py
@@ -1,12 +1,16 @@
 import arcade
+import time
+from pyglet.math import Vec2
 
 
 class Player:
     def __init__(self, x=0, y=0, sprite=None):
         self.playerSprite = sprite
-        self.movement_speed = 2
+        self.movement_speed = 1.5
+        self.sprint_speed = 3
         self.dodge_distance = 30
         self.dodge_cooldown = 3
+        self.doge_last_time = time.perf_counter() - self.dodge_cooldown
         self.x = x
         self.y = y
         self.playerSprite.center_x = self.x
@@ -29,6 +33,86 @@ class Player:
         """Update Pozycji Gracza"""
         self.x = self.playerSprite.center_x
         self.y = self.playerSprite.center_y
+
+    def movement(self, camera, camer_speed, width, height, physics_engine):
+        """Pełny Ruch Gracza"""
+        def check_move_key():  # aktualizacja pozycji w zależności od naciśniętych klawiszy
+            if arcade.key.LSHIFT in self.keys:
+                speed = self.sprint_speed
+            else:
+                speed = self.movement_speed
+
+            if arcade.key.W in self.keys:
+                self.playerSprite.change_y = speed
+                self.direction_move = "Up"
+            elif arcade.key.S in self.keys:
+                self.playerSprite.change_y = -speed
+                self.direction_move = "Down"
+            else:
+                self.playerSprite.change_y = 0
+
+            if arcade.key.A in self.keys:
+                self.playerSprite.change_x = -speed
+                if arcade.key.W in self.keys:
+                    self.direction_move = "UpLeft"
+                elif arcade.key.S in self.keys:
+                    self.direction_move = "DownLeft"
+                else:
+                    self.direction_move = "Left"
+            elif arcade.key.D in self.keys:
+                self.playerSprite.change_x = speed
+                if arcade.key.W in self.keys:
+                    self.direction_move = "UpRight"
+                elif arcade.key.S in self.keys:
+                    self.direction_move = "DownRight"
+                else:
+                    self.direction_move = "Right"
+            else:
+                self.playerSprite.change_x = 0
+
+            if arcade.key.SPACE in self.keys:
+                if time.perf_counter() - self.doge_last_time > self.dodge_cooldown:
+                    dodge()
+
+        def move_camera_to_player():
+            position = Vec2(
+                self.playerSprite.center_x - width / 2,
+                self.playerSprite.center_y - height / 2
+            )
+            camera.move_to(position, camer_speed)
+
+        def dodge():
+            self.doge_last_time = time.perf_counter()
+            if self.direction_move == "Left":
+                self.playerSprite.change_x = -self.movement_speed * self.dodge_distance
+            elif self.direction_move == "UpLeft":
+                self.playerSprite.change_y = self.movement_speed * self.dodge_distance
+                self.playerSprite.change_x = -self.movement_speed * self.dodge_distance
+            elif self.direction_move == "Up":
+                self.playerSprite.change_y = self.movement_speed * self.dodge_distance
+
+            elif self.direction_move == "UpRight":
+                self.playerSprite.change_y = self.movement_speed * self.dodge_distance
+                self.playerSprite.change_x = self.movement_speed * self.dodge_distance
+
+            elif self.direction_move == "Right":
+                self.playerSprite.change_x = self.movement_speed * self.dodge_distance
+
+            elif self.direction_move == "DownRight":
+                self.playerSprite.change_y = -self.movement_speed * self.dodge_distance
+                self.playerSprite.change_x = self.movement_speed * self.dodge_distance
+
+            elif self.direction_move == "Down":
+                self.playerSprite.change_y = -self.movement_speed * self.dodge_distance
+
+            elif self.direction_move == "DownLeft":
+                self.playerSprite.change_y = -self.movement_speed * self.dodge_distance
+                self.playerSprite.change_x = -self.movement_speed * self.dodge_distance
+
+        check_move_key()
+        physics_engine.update()
+        self.update_pos()
+        move_camera_to_player()
 
     def is_alive(self):
         if self.health > 0:
