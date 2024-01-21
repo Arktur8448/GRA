8a3f2ee5e09866a755abf66a1fa0c34e5e9f0484
Cytronizyn <130798772+Cytronizyn@users.noreply.github.com>
2024-01-21 14:04:04 +0100

    Nie brunetki nie blondynki ja wole Dude

--- items.py
+++ items.py
@@ -9,46 +9,53 @@ class Item:
 
 
 class Food(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, level_of_fullness, consumable):
-        super().__init__(item_rarity, name, price_buy, price_sell)
-        self.level_of_fullness = level_of_fullness
+    def __init__(self, sprite_path, description, name, item_rarity=0, price_buy=0, price_sell=0, regen_health=0, regen_mana=0, time_regen=0, consumable=True):
+        super().__init__(sprite_path, description, name, item_rarity, price_buy, price_sell)
+        self.regen_health = regen_health
+        self.regen_mana = regen_mana
+        self.time_regen = time_regen
         self.consumable = consumable
 
-    def check_fullness(self, player_fullness):
-        if player_fullness > 0:
+
+class Upgrader(Item):
+    def __init__(self, sprite_path, description, name, item_rarity, price_buy, price_sell, special_effect, lvl_upgrader):
+        super().__init__(sprite_path, description, name, item_rarity,  price_buy, price_sell)
+        self.special_effect = special_effect if special_effect else []
+        self.lvl_upgrader = lvl_upgrader
+
+    def check_lvl(self, player_lvl):
+        if player_lvl >= self.lvl_upgrader:
             return True
         else:
             return False
 
 
-class WeaponUpgrader(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, type_weapon, special_effect):
-        super().__init__(item_rarity, name, price_buy, price_sell)
-        self.type_weapon = type_weapon
-        self.special_effect = special_effect
-
-
-class ArmorUpgrader(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, type_armor, special_effect):
-        super().__init__(item_rarity, name, price_buy, price_sell)
-        self.type_armor = type_armor
-        self.special_effect = special_effect
+class Book(Upgrader):
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, special_effect = None, lvl_upgrader=1, type_item="Weapon"):
+        super().__init__(sprite_path, description, name, item_rarity,  price_buy, price_sell, special_effect, lvl_upgrader)
+        self.type_item = type_item
 
 
 class Ore(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, mining_lucky):
-        super().__init__(item_rarity, name, price_buy, price_sell)
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, mining_lucky=0, lvl_ore=1):
+        super().__init__(sprite_path, description, name, item_rarity, price_buy, price_sell)
         self.mining_lucky = mining_lucky
+        self.lvl_ore = lvl_ore
 
     def extralucky(self, lucky_lvl):
         self.mining_lucky *= lucky_lvl
 
+    def check_lvl(self,player_lvl):
+        if player_lvl >= self.lvl_ore:
+            return True
+        else:
+            return False
+
 
 class Key(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, destination, special_effect, key_lvl):
-        super().__init__(item_rarity, name, price_buy, price_sell)
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, destination=0, key_lvl=1):
+        super().__init__(sprite_path, description, name, item_rarity, price_buy, price_sell)
         self.destination = destination
-        self.special_effect = special_effect
         self.key_lvl = key_lvl
 
     def check_lvl(self, player_lvl):
@@ -67,16 +74,16 @@ class Key(Item):
 
 
 class Potion(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, health_gain, mana_gain, uses):
-        super().__init__(item_rarity, name, price_buy, price_sell)
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, health_gain=0, mana_gain=0, uses=0):
+        super().__init__(sprite_path, description, name, item_rarity, price_buy, price_sell)
         self.health_gain = health_gain
         self.mana_gain = mana_gain
         self.uses = uses
 
 
 class Accessories(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory):
-        super().__init__(item_rarity, name, price_buy, price_sell)
+    def __init__(self, sprite_path, description,  item_rarity, name, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory):
+        super().__init__(sprite_path, description, item_rarity, name, price_buy, price_sell)
         self.special_effect = special_effect
         self.extra_health = extra_health
         self.extra_mana = extra_mana
@@ -92,23 +99,23 @@ class Accessories(Item):
 
 
 class Ring(Accessories):
-    def __init__(self, item_rarity, name, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory):
-        super().__init__(item_rarity, name, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory)
+    def __init__(self, sprite_path, description, name,  item_rarity="Common", price_buy=0, price_sell=0, special_effect=0, extra_health=0, extra_mana=0, extra_damage=0, extra_defence=0, lvl_accessory=1):
+        super().__init__(sprite_path, description, name, item_rarity, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory)
 
 
 class Amulet(Accessories):
-    def __init__(self, item_rarity, name, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory):
-        super().__init__(item_rarity, name, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory)
+    def __init__(self, sprite_path, description, name,  item_rarity="Common", price_buy=0, price_sell=0, special_effect=0, extra_health=0, extra_mana=0, extra_damage=0, extra_defence=0, lvl_accessory=1):
+        super().__init__(sprite_path, description, name, item_rarity, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory)
 
 
 class Necklace(Accessories):
-    def __init__(self, item_rarity, name, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory):
-        super().__init__(item_rarity, name, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory)
+    def __init__(self, sprite_path, description, name,  item_rarity="Common", price_buy=0, price_sell=0, special_effect=0, extra_health=0, extra_mana=0, extra_damage=0, extra_defence=0, lvl_accessory=1):
+        super().__init__(sprite_path, description, name, item_rarity, price_buy, price_sell, special_effect, extra_health, extra_mana, extra_damage, extra_defence, lvl_accessory)
 
 
 class Weapon(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, damage, speed , lvl_weapon):
-        super().__init__(item_rarity, name, price_buy, price_sell)
+    def __init__(self, sprite_path, description,  item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, damage, speed , lvl_weapon):
+        super().__init__(sprite_path, description, item_rarity, name, price_buy, price_sell)
         self.strength_requirement = strength_requirement
         self.agility_requirement = agility_requirement
         self.damage = damage
@@ -133,36 +140,39 @@ class Weapon(Item):
 
 
 class Sword(Weapon):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement,
-                 damage, speed, slash_damage, slash_damage_mana_requirement, lvl_weapon):
-        super().__init__(item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement,
-                         damage, speed, lvl_weapon)
-        self.slash_damage = slash_damage
-        self.slash_damage_mana_requirement = slash_damage_mana_requirement
-
-
-class Axe(Weapon):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement,
-                 damage, speed, chop_damage, chop_damage_mana_requirement, lvl_weapon):
-        super().__init__(item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement,
-                         damage, speed, lvl_weapon)
-        self.chop_damage = chop_damage
-        self.chop_damage_mana_requirement = chop_damage_mana_requirement
-
-
-class Lance(Weapon):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement,
-                 damage, speed, charge_damage, charge_damage_mana_requirement, lvl_weapon):
-        super().__init__(item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement,
+    def __init__(self, sprite_path, description, name, item_rarity=0, price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0,
+                 damage=0, speed=0, charge_damage=0, charge_damage_mana_requirement=0, lvl_weapon=1, special_effect=None):
+        super().__init__(sprite_path, description, name, item_rarity,price_buy, price_sell, strength_requirement, agility_requirement,
                          damage, speed, lvl_weapon)
         self.charge_damage = charge_damage
         self.charge_damage_mana_requirement = charge_damage_mana_requirement
+        self.special_effect = special_effect if special_effect else []
+
+
+class Axe(Weapon):
+    def __init__(self, sprite_path, description, name, item_rarity=0, price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0,
+                 damage=0, speed=0, charge_damage=0, charge_damage_mana_requirement=0, lvl_weapon=1, special_effect=None):
+        super().__init__(sprite_path, description, name, item_rarity,price_buy, price_sell, strength_requirement, agility_requirement,
+                         damage, speed, lvl_weapon)
+        self.charge_damage = charge_damage
+        self.charge_damage_mana_requirement = charge_damage_mana_requirement
+        self.special_effect = special_effect if special_effect else []
+
+
+class Lance(Weapon):
+    def __init__(self, sprite_path, description, name, item_rarity=0, price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0,
+                 damage=0, speed=0, charge_damage=0, charge_damage_mana_requirement=0, lvl_weapon=1, special_effect=None):
+        super().__init__(sprite_path, description, name, item_rarity,price_buy, price_sell, strength_requirement, agility_requirement,
+                         damage, speed, lvl_weapon)
+        self.charge_damage = charge_damage
+        self.charge_damage_mana_requirement = charge_damage_mana_requirement
+        self.special_effect = special_effect if special_effect else []
 
 
 class Armor(Item):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement,
+    def __init__(self, sprite_path, description, name, item_rarity, price_buy, price_sell, strength_requirement, agility_requirement,
                  defence, lvl_armor):
-        super().__init__(item_rarity, name, price_buy, price_sell)
+        super().__init__(sprite_path, description, name, item_rarity, price_buy, price_sell)
         self.strength_requirement = strength_requirement
         self.agility_requirement = agility_requirement
         self.defence = defence
@@ -185,43 +195,55 @@ class Armor(Item):
 
 
 class Hat(Armor):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, special_effect,
-                 lvl_armor):
-        super().__init__(item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
-        self.special_effect = special_effect
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0, defence=0, special_effect=None,
+                 lvl_armor=1):
+        super().__init__(sprite_path, description, name, item_rarity,price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
+        self.special_effect = special_effect if special_effect else []
 
 
 class ChestPlate(Armor):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, special_effect,
-                 lvl_armor):
-        super().__init__(item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
-        self.special_effect = special_effect
-
-
-class Pants(Armor):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, special_effect,
-                 lvl_armor):
-        super().__init__(item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
-        self.special_effect = special_effect
-
-
-class Shoes(Armor):
-    def __init__(self, item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, special_effect,
-                 lvl_armor):
-        super().__init__(item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
-        self.special_effect = special_effect
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0, defence=0, special_effect=None,
+                 lvl_armor=1):
+        super().__init__(sprite_path, description, name, item_rarity,price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
+        self.special_effect = special_effect if special_effect else []
 
 
 class Gloves(Armor):
-    def __init__(self, name, item_rarity="Common", price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0, defence=0, special_effect=[],
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0, defence=0, special_effect=None,
                  lvl_armor=1):
-        super().__init__(item_rarity, name, price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
-        self.special_effect = special_effect
+        super().__init__(sprite_path, description, name, item_rarity,price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
+        self.special_effect = special_effect if special_effect else []
+
+
+class Pants(Armor):
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0, defence=0, special_effect=None,
+                 lvl_armor=1):
+        super().__init__(sprite_path, description, name, item_rarity,price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
+        self.special_effect = special_effect if special_effect else []
+
+
+class Shoes(Armor):
+    def __init__(self, sprite_path, description, name, item_rarity="Common", price_buy=0, price_sell=0, strength_requirement=0, agility_requirement=0, defence=0, special_effect=None,
+                 lvl_armor=1):
+        super().__init__(sprite_path, description, name, item_rarity,price_buy, price_sell, strength_requirement, agility_requirement, defence, lvl_armor)
+        self.special_effect = special_effect if special_effect else []
+
 
 
 # speed = co ile sekund atak od momentu kliknienioa przycisku
 # template : Sword_blahblah = Sword("sword", "Sword_blahblah", False, 500, 400, 25, 10, 30, 2, 50, 25)
 # items = [None, Sword_blahblah, axe_coścoś, itp...]
+items = [
+    Food("", "", "Apple", 1, 10, 5, 2, 0, 3, True),
+    Key("", "", "Golden Key", "Rare", 150, 100, "Door", 1),
+    Ore("", "", "Iron", "Common", 30, 20, 6, 1),
+    Book(" ", " ", "Spellbook of Fire", "Rare", 200, 150, [None, "fire resistant"], 3, "Armor"),
+    Potion(" ", " ", "Small Health Potion", "Common", 50, 40, 10, 0, 1)
+]
+
+
+
+
 
 
 
