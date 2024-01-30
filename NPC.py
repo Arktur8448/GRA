c06310a133899e0dbaef6e434db8e0bfc8c49d71
Cytronizyn <130798772+Cytronizyn@users.noreply.github.com>
2024-01-30 14:06:28 +0100

    npc ziuty

--- NPC.py
+++ NPC.py
@@ -1,10 +1,10 @@
 import arcade
 import items
 import inventory
-
+from races import Friend
 
 class NPC (arcade.Sprite):
-    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1, race=None,
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
                  weapon=None, drop=None, to_sell=None):
         super().__init__(filename=filename, center_x=pos_x, center_y=pos_y, scale=1.6)
         self.name = name
@@ -16,6 +16,7 @@ class NPC (arcade.Sprite):
         self.strength = strength
         self.defence = defence
         self.agility = agility
+        self.speed = speed
         self.race = race
         self.weapon = weapon if weapon else []
         self.drop = drop if drop else []
@@ -39,7 +40,136 @@ class NPC (arcade.Sprite):
         )
         return hp
 
-# class Mage(NPC):
-#     pass
-#     def use_spell(self, player):
-#         pass
+
+class Tradesman(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
+                 weapon=None, drop=None, to_sell=None):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop, to_sell)
+    def buy_item(self, item_index, buyer):
+        if 0 <= item_index < len(self.to_sell):
+            item, price = self.to_sell[item_index]
+            if buyer.gold >= price:
+                buyer.gold -= price
+                buyer.inventory.append(item)
+                return f"You bought {item.name} for {price} Gold."
+            else:
+                return "You don't have enough Gold to buy this item."
+        else:
+            return "Invalid item index."
+
+    def sell_item(self, item_index, seller):
+        item,price = self.to_sell[item_index]
+        seller.gold += (price * 0.8)
+        seller.inventory.remove(item)
+
+
+class Smith(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
+                 weapon=None, drop=None, to_sell=None):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop, to_sell)
+
+    def buy_item(self, item_index, buyer):
+        if 0 <= item_index < len(self.to_sell):
+            item, price = self.to_sell[item_index]
+            if buyer.gold >= price:
+                buyer.gold -= price
+                buyer.inventory.append(item)
+                return f"You bought {item.name} for {price} Gold."
+            else:
+                return "You don't have enough Gold to buy this item."
+        else:
+            return "Invalid item index."
+
+    def upgrader(self,item,book_effect,book_price,player):
+        if player.gold >= book_price:
+            player.gold -= book_price
+            item.special_effect.append(book_effect)
+
+    def dropupgrade(self,item,book_effect,book_price,player):
+        if player.gold >= (book_price * 0.4):
+            player.gold -= (book_price * 0.4)
+            item.special_effect.remove(book_effect)
+
+
+class Alchemist(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
+                 weapon=None, drop=None, to_sell=None):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop, to_sell)
+
+    def use_spell(self,player):
+        pass
+
+    def buy_item(self, item_index, buyer):
+        if 0 <= item_index < len(self.to_sell):
+            item, price = self.to_sell[item_index]
+            if buyer.gold >= price:
+                buyer.gold -= price
+                buyer.inventory.append(item)
+                return f"You bought {item.name} for {price} Gold."
+            else:
+                return "You don't have enough Gold to buy this item."
+        else:
+            return "Invalid item index."
+
+
+class EnemyShortDistance(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
+                 weapon=None, drop=None,armor=None,s):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop)
+        self.armor = armor if armor else []
+
+
+class EnemyLongDistance(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
+                 weapon=None, drop=None,armor=None,s):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop)
+        self.armor = armor if armor else []
+
+
+class HevyEnemy(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
+                 weapon=None, drop=None,armor=None,s):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop)
+        self.armor = armor if armor else []
+        self.speed = speed
+
+
+class Mage(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
+                 weapon=None, drop=None,armor=None,magic=None):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop,)
+        self.armor = armor if armor else []
+        self.magic = magic if magic else []
+
+    def use_spell(self,player):
+        pass
+
+
+class Boss(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=None,
+                 weapon=None, drop=None,armor=None,act=1):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop)
+        self.armor = armor if armor else []
+        self.act = act
+
+    def check_act(self,player):
+        if player.act == self.act:
+            return True
+        else:
+            return False
+
+
+class Friends(NPC):
+    def __init__(self, filename, name, description, pos_x, pos_y, level=1, exp_drop=0, health=1, strength=1, defence=1, agility=1,speed=1, race=Friend,
+                 weapon=None, drop=None,act=1,armor=None):
+        super().__init__(filename, name, description, pos_x, pos_y, level, exp_drop, health, strength, defence, agility,speed, race,weapon, drop)
+        self.act = act
+        self.armor = armor if armor else []
+        #to taki ziutek co daje ci misje na poczatku i on rozpoczyna fabule
+
+    def check_act(self,player):
+        if player.act == self.act:
+            return True
+        else:
+            return False
+
--- races.py
+++ races.py
@@ -1,13 +1,37 @@
 class Race:
-    def __init__(self, name, health, mana,strength, defence, agility):
+    def __init__(self, name,description, health, mana,strength, defence, agility,stamina,):
         self.name = name
+        self.description = description
         self.health = health
         self.mana = mana
         self.strength = strength
         self.defence = defence
         self.agility = agility
+        self.stamina = stamina
 
 
-Human = Race("Human", 25, 5, 10, 5, 5)
+class NonPlayableRace():
+    def __init__(self,name,description,health,mana,strength,agility,stamina):
+        self.name = name
+        self.description = description
+        self.health = health
+        self.mana = mana
+        self.strength = strength
+        self.agility = agility
+        self.stamina = stamina
+
+
+Human = Race("Human", "Wszechstronne istoty zdolne do różnych profesji i umiejętności.", 25, 10, 10, 10, 5, 10)
+Elf = Race("Elf", "Długowieczne istoty o zwinności i smukłej budowie, często utalentowane w magii i sztukach walki.", 20, 15, 3, 5, 10, 15)
+Dwarf = Race("Dwarf", "Mocne i wytrzymałe istoty o umiejętnościach rzemieślniczych, często mieszkające w górach.", 20, 5, 8, 5, 5, 5)
+Orc = Race("Orc", "Silne i agresywne istoty, znane ze swojej siły fizycznej i zdolności do walki.", 40, 20, 15, 10, 3, 15)
+Vampire = Race("Vampire", "Nieumarłe istoty, które żywią się krwią, posiadające różnorodne zdolności nadnaturalne.", 25, 10, 5, 5, 10, 5)
+Drakonid = Race("Drakonid", "Pół-człowiek, pół-smok, posiadający zdolności związane z ogniem, lodem lub innymi elementami, w zależności od rodzaju smoka.", 40, 20, 10, 10, 10, 15)
+Titan = NonPlayableRace("Titan", "Ogromne istoty, które kiedyś rządziły światem, obecnie uwięzione w odległych zakątkach świata.", 100, 50, 30, 20, 20)
+Skeleton = NonPlayableRace("Skeleton", "Słabe istoty.", 15, 10, 5, 5, 5)
+Griffin = Race("Griffin", "Potomkowie gryfonów, dobrzy łucznicy.", 30, 5, 5, 15, 3, 10)
+Ghost = NonPlayableRace("Ghost", "Potrafią zniknąć.", 20, 20, 2, 15, 10)
+Friend = NonPlayableRace("Friend","git ziut",25,10,10,10,10)
+
 
 # races = [Human, Elf, itp...]
